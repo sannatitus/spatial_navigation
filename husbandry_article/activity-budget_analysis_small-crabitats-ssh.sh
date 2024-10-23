0962c2ae2d36cc685b96e47a5fb2a80d9ef44eae
Sanna Eriksson Titus <s.titus@ucl.ac.uk>
2024-10-23 16:08:59 +0100

    Delete husbandry_article/activity-budget_analysis_small-crabitats-ssh.sh

--- husbandry_article/activity-budget_analysis_small-crabitats-ssh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=crabs-activity                  # job name
-#SBATCH --partition=cpu                            # partition (queue)
-#SBATCH --constraint="GenuineIntel-6-79"           # request nodes with AVX2 support
-#SBATCH --nodes=1                                  # node count
-#SBATCH --mem=8G                                   # total memory per node
-#SBATCH --time=0-5:00:00                           # total run time limit (DD-HH:MM:SS)
-python activity-budget_small-crabitats.py
